559a64cf55d24a5163359894fc42bdd82caa1b81
Krzysztof Szabelski <kszabelski@gmail.com>
2016-03-21 00:18:15 +0100

    Simplify layout to fit on map screen

--- PlayEndomondoOnGoogleMaps/Scripts/mapsite.js
+++ PlayEndomondoOnGoogleMaps/Scripts/mapsite.js
@@ -3,12 +3,13 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 3,
         center: { lat: 0, lng: 0 },
-        mapTypeId: google.maps.MapTypeId.TERRAIN
+        //mapTypeId: google.maps.MapTypeId.TERRAIN
+        mapTypeId: google.maps.MapTypeId.HYBRID
     });
 
     $("#btnCenter").click(function() {
         map.setZoom(parseInt($("#inputZoom").val()));
-        map.setCenter(coords[0]);
+        map.setCenter(coords[Math.floor(coords.length / 2)]);
     });
 
     $("#btnDraw").click(function () {
@@ -23,7 +24,7 @@ function initMap() {
                     geodesic: true,
                     strokeColor: '#FF0000',
                     strokeOpacity: 1.0,
-                    strokeWeight: 2
+                    strokeWeight: 4
                 });
 
                 path.setMap(map);
@@ -31,7 +32,7 @@ function initMap() {
         }
 
         for (var i = 0; i < coords.length; i++) {
-            window.setTimeout(createTimeoutCall(i), i * 50)
+            window.setTimeout(createTimeoutCall(i), i * 25)
         }
     });
 }
