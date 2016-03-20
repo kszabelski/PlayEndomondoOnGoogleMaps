4931ecaa764c530467265d6bc9174f22a1ac5991
Krzysztof Szabelski <kszabelski@gmail.com>
2016-03-20 16:26:10 +0100

    MVC template cleared

---
+++ PlayEndomondoOnGoogleMaps/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlayEndomondoOnGoogleMaps.Startup))]
+namespace PlayEndomondoOnGoogleMaps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
