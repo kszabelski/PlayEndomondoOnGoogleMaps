c9a020f0ac0b86a7800395f02962434fd1f9b658
Krzysztof Szabelski <kszabelski@gmail.com>
2016-03-20 22:17:17 +0100

    Implemented displaying track on map

--- PlayEndomondoOnGoogleMaps/Controllers/HomeController.cs
+++ PlayEndomondoOnGoogleMaps/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
+using System.Web.Http.Tracing;
 using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace PlayEndomondoOnGoogleMaps.Controllers
 {
@@ -10,7 +14,27 @@ namespace PlayEndomondoOnGoogleMaps.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Coords = "";
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            ViewBag.Coords = "";
+            if (file != null && file.ContentLength > 0)
+            {
+                var root = XElement.Load(file.InputStream);
+                var ns = $"{{{root.GetDefaultNamespace().NamespaceName}}}";
+                var elements = root.Elements(ns + "trk").Elements(ns + "trkseg").Elements(ns + "trkpt").ToList();
+                var stringElements = elements.Select(e => $"{{lat:{e.Attribute("lat").Value},lng:{e.Attribute("lon").Value}}}");
+                ViewBag.Coords = $"[{string.Join(",", stringElements)}]";
+            }
+
+            return View();
+        }
+
+
+
     }
 }
---
+++ PlayEndomondoOnGoogleMaps/Scripts/mapsite.js
@@ -0,0 +1,37 @@
+﻿var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 3,
+        center: { lat: 0, lng: 0 },
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    });
+
+    $("#btnCenter").click(function() {
+        map.setZoom(parseInt($("#inputZoom").val()));
+        map.setCenter(coords[0]);
+    });
+
+    $("#btnDraw").click(function () {
+        if (!coords) {
+            return;
+        }
+
+        function createTimeoutCall(i) {
+            return function () {
+                var path = new google.maps.Polyline({
+                    path: [coords[i], coords[i + 1]],
+                    geodesic: true,
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 1.0,
+                    strokeWeight: 2
+                });
+
+                path.setMap(map);
+            }
+        }
+
+        for (var i = 0; i < coords.length; i++) {
+            window.setTimeout(createTimeoutCall(i), i * 50)
+        }
+    });
+}
